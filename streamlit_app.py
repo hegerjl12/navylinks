466c59330719a13dc9ba868ab25e1d4aa2349893
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-07-28 15:00:50 -0700

    ASM and coffee only

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,12 @@ from PIL import Image
 
 def main():
   
+  st.set_page_config(
+     page_title="Navy Links",
+     page_icon="🔗",
+     layout="wide",
+)
+  
   load_css()
 
   col1, col2, col3 = st.columns(3)
@@ -16,15 +22,11 @@ def main():
   st.info('Useful Webpage Links')
 
   icon_size = 20
-
-  st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-  st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-  st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-  st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-  st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-  st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-  st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+  
   st_button('clipboard', 'https://asm3.nmci.navy.mil/ASM3/cac?utm_source=mnp%20public', 'ASM', icon_size)
+
+  st_button('cup', 'https://www.buymeacoffee.com/hegerjl', 'Buy me a Coffee', icon_size)
+  
 
 if __name__ == "__main__":
   main()
