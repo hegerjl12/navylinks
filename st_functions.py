9f5e81b04b84fd04a71bbe1622be47c029831745
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-07-29 11:00:51 -0700

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -126,7 +126,18 @@ def st_button(icon, url, label, iconsize):
                 </svg>
                 {label}
             </a>
-        </p>'''    
+        </p>'''
+    elif icon == 'health':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-prescription2" viewBox="0 0 16 16">
+                    <path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2V6Z"/>
+                    <path fill-rule="evenodd" d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3h8v10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5V4ZM3 3V1h10v2H3Z"/>
+                </svg>
+                {label}
+            </a>
+        </p>'''
     elif icon == '':
         button_code = f'''
         <p>
