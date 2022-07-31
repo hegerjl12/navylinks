fd5f8610bd27b94a2222751cd22fa48465722f68
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-07-31 11:06:15 -0700

    test

--- st_functions.py
+++ st_functions.py
@@ -74,7 +74,7 @@ def st_button(icon, url, label, iconsize):
                     <path d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1Zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5v-1Zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2Z"/>
                 </svg>
                 {label}
-                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="24" fill="currentColor" class="bi bi-lock-fill" viewBox="0 16 17 12">
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="24" fill="currentColor" class="bi bi-lock-fill" viewBox="0 15 17 12">
                     <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
                 </svg>
             </a>
