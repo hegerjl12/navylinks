cbe8bfffa25275d40f2189ab67c66178fcb682dc
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-07-31 11:10:49 -0700

    test

--- st_functions.py
+++ st_functions.py
@@ -74,8 +74,8 @@ def st_button(icon, url, label, iconsize):
                     <path d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1Zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5v-1Zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2Z"/>
                 </svg>
                 {label}
-                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="24" fill="currentColor" class="bi bi-lock-fill" viewBox="0 14 17 12">
-                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="24" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 14 17 12">
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
                 </svg>
             </a>
         </p>'''
