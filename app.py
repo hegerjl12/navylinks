ebd1b596456eef7acedcd8fc25ab91de79d817c6
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-08-11 13:00:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,51 +4,10 @@ import numpy as np
 from st_functions import st_button, load_css
 from PIL import Image
 import streamlit.components.v1 as components
-import pathlib
-from bs4 import BeautifulSoup
-import logging
-import shutil
 import os
 import re
 
-def inject_ga():
-    GA_ID = "google_analytics"
-
-    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
-    # web application's id is. You will find this in your Google Analytics account
-    
-    GA_JS = '''<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ECZ57RBDL6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ECZ57RBDL6');
-</script>
-
-<a title="Privacy-friendly Web Analytics" href="https://clicky.com/101376338"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script async src="//static.getclicky.com/101376338.js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101376338ns.gif" /></p></noscript>
-
-<script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>'''
-
-
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):  # if cannot find tag
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # recover from backup
-        else:
-            shutil.copy(index_path, bck_index)  # keep a backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
-
-
+   
 def main():
 
   st.set_page_config(
@@ -56,11 +15,13 @@ def main():
      page_icon="🔗",  
   )
   
-  #inject_ga()
+
 
   code = '''<a title="Privacy-friendly Web Analytics" href="https://clicky.com/101376338"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
 <script async src="//static.getclicky.com/101376338.js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101376338ns.gif" /></p></noscript>'''
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101376338ns.gif" /></p></noscript>
+
+<script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>'''
     
   a=os.path.dirname(st.__file__)+'/static/index.html'
   with open(a, 'r') as f:
