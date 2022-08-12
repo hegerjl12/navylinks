f77d9448d7672382c0173ff35f4369dcc877fce4
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-08-11 20:29:47 -0700

    try microanalytics

--- app.py
+++ app.py
@@ -15,13 +15,11 @@ def main():
      page_icon="🔗",  
   )
   
+#<a title="Privacy-friendly Web Analytics" href="https://clicky.com/101376338"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+#<script async src="//static.getclicky.com/101376338.js"></script>
+#<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101376338ns.gif" /></p></noscript>
 
-
-  code = '''<a title="Privacy-friendly Web Analytics" href="https://clicky.com/101376338"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script async src="//static.getclicky.com/101376338.js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101376338ns.gif" /></p></noscript>
-
-<script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>'''
+  code = '''<script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>'''
     
   a=os.path.dirname(st.__file__)+'/static/index.html'
   with open(a, 'r') as f:
