529ecebb14fde66717e0567e0685c818e0848f1c
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-12-14 14:19:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def main():
     st.write(taf.raw)
     st.write(notam.update())
     st.write(notam.last_updated)
-    st.write(notam.data.raw)
+    st.write(notam.data[1].raw)
     
       
   with col2:
