a250da3ff9ecf631ed067da0b5055a0ac2c939d5
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-12-14 14:52:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
     for i in range(len(notam.data)):
       st.write(notam.data[i].raw)
         
-    components.iframe('https://embed.windy.com/embed2.html?lat=48.210&lon=-122.695&detailLat=37.751&detailLon=-97.822&width=650&height=450&zoom=6&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=kt&metricTemp=%C2%B0F&radarRange=-1') 
+    components.html('<iframe width="650" height="450" src="https://embed.windy.com/embed2.html?lat=48.210&lon=-122.695&detailLat=37.751&detailLon=-97.822&width=650&height=450&zoom=6&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=kt&metricTemp=%C2%B0F&radarRange=-1" frameborder="0"></iframe>') 
    
   with col2:
 
