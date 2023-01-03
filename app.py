cc056335f886f3dff84668060f3a98a3fda59ffa
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-01-03 14:45:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ from PIL import Image
 import os
 import avwx
 import streamlit.components.v1 as components
+import base64
+   
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )   
    
 def main():
 
@@ -15,6 +31,8 @@ def main():
   )
 
   load_css()
+   
+  add_bg_from_local('growler.png')
 
   st.title('SDO')
 
